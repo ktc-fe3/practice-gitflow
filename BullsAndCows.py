2b0b94db730d80dc067bb2879195530483878dca
kjh302903 <kjh302903@gmail.com>
2023-05-01 19:58:11 +0900

    fix: Fix createRandom function
    createRandom 함수의 리턴값을 string type에서 int type으로 수정

--- BullsAndCows.py
+++ BullsAndCows.py
@@ -3,6 +3,5 @@ import itertools
 
 def createRandom():
     randomList = [''.join(x) for x in list(itertools.permutations('0123456789',4))]
-    return random.choice(randomList)
+    return int(random.choice(randomList))
 
-print(createRandom())
